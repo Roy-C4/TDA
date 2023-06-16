d5555aa8bdd3fbfd1232698ea856dfc149419461
daniel <henkhamerman@gmail.com>
2023-06-16 14:57:25 +0200

    pushy

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ from datetime import datetime
 from scipy.spatial import distance_matrix
 from tqdm import tqdm
 
+
 # Converts longitude or latitude values to meters
 def to_meters(longs_or_lats):
     return longs_or_lats * 111139.0
+
 
 # Define the projection map
 proj = pyproj.Proj(proj='utm', zone=50, ellps='WGS84')
@@ -39,7 +41,7 @@ df = df[lat_long_mask]
 
 # Filter the data on daytime traffic
 df['Date_Time'] = pd.to_datetime(df['Date_Time'])
-daytime_mask = (df['Date_Time'].dt.hour >= 9) & (df['Date_Time'].dt.hour < 17) # from 09.00 to 17.00
+daytime_mask = (df['Date_Time'].dt.hour >= 6) & (df['Date_Time'].dt.hour < 10)  # from 06.00 to 10.00
 df = df[daytime_mask]
 
 # Filter the data on car traffic
@@ -54,17 +56,36 @@ df_new = pd.DataFrame(columns=['Long_meters', 'Lat_meters', 'Altitude', 'Longitu
 
 
 # Filter the data on points that are close to each other
+c = 0
+# done = False
 for _, row in tqdm(df.iterrows(), total=df.shape[0]):
+    # if done:
+    #     break
+
+    # Denotes that a point does not collide with any of the already existing points within 50 meters
+    eligible = True
     for _, new_row in df_new.iterrows():
-        if abs(new_row['Long_meters'] - row['Long_meters']) < 50 and abs(new_row['Lat_meters'] - row['Lat_meters']) < 50:
+        if np.abs(new_row['Long_meters'] - row['Long_meters']) < 50 and np.abs(new_row['Lat_meters'] - row['Lat_meters']) < 50:
+            eligible = False
             break
-    else:
-        df_new.loc[len(df_new)] = [row['Long_meters'], row['Lat_meters'], 0, row['Longitude'], row['Latitude']]
+
+    # Check if data point does not clash with any of the other present data points
+    if eligible:
+        df_new.loc[len(df_new)] = [row['Long_meters'], row['Lat_meters'], row['Altitude'], row['Longitude'], row['Latitude']]
+        # c += 1
+
+        # if c % 100 == 0:
+        #     print(c)
+        # if c >= 500:
+        #     done = True
 
 df = df_new
+df.to_csv('filtered_trajectories.csv', index=False)
+
 print(df.head(100))
+
 
 # Visualize the filtered points
 x, y = proj(df['Longitude'].tolist(), df['Latitude'].tolist())
-p = np.c_[x, y, 0.3048 * df['Altitude']] # convert alt to meters
+p = np.c_[x, y, 0.3048 * df['Altitude']]  # convert alt to meters
 v = pptk.viewer(p)
